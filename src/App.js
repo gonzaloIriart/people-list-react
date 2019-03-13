3999765b31943e0eef7d0deab7476219f0c9b2e9
Gonzalo <gonzaloiriart@gmail.com>
2019-03-13 11:00:48 -0300

    finish practice, could improve css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import './App.css';
 
-const Person = () =>{
-const url = "https://randomuser.me/api/portraits/thumb/men/01.jpg";
-return <div className="person">
-  I am person component
-  </div>};
+const Person = (props) =>{
+ const {img,name,job} = props.person;
+ const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return <div className="person">
+      <img src = {url} alt = "thumnail"/>
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        <p>{children}</p>
+      </div>
+    </div>}
+
 
 const PersonList = () =>{
-const people = [{}];
-
+const people = [
+  {
+    img: 22,name :"George",  job:"developer"
+},
+{
+    img: 10,name : "Joseph",  job : "designer"
+},
+{
+  img: 30,name : "John",  job : "artist"
+},
+{
+}
+];
   return(
-    <Person></Person>
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]}>
+      lorem ipsumm dolor
+      </Person>
+      <Person person={people[2]} />
+    </section>
+
   )
+    
+  
+
+  
 };
 
 const App = () => (<PersonList />);
